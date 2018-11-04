dbf2f085c9af0423cd937edb13cc6d0893bbffc4
Paumila <pau.milaguasch@gmail.com>
2018-11-04 16:04:01 +0100

    Tutorial 1

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pau/Escritorio/Master en Robotica/Robotics Integration/ros_tutorials/catkin_ws/src
+# Build directory: /home/pau/Escritorio/Master en Robotica/Robotics Integration/ros_tutorials/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
