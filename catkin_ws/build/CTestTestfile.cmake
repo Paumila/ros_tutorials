574846d1aab680da949f7482e11b22218968652b
Paumila <pau.milaguasch@gmail.com>
2018-11-07 20:46:02 +0100

    Borrant carpetes Devel/Install/Build

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pau/Escritorio/Master en Robotica/Robotics Integration/ros_tutorials/catkin_ws/src
-# Build directory: /home/pau/Escritorio/Master en Robotica/Robotics Integration/ros_tutorials/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("beginner_tutorials")
-subdirs("beginner_tutorials1")
